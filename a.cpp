9d5dc315172e9639d17456434de5976bcde84131
DatNT <tpc2102@gmail.com>
2018-09-20 14:22:42 +0700

    timus1013

--- a.cpp
+++ a.cpp
@@ -1,69 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define maxN 20000
-
-typedef pair<pair<int, int>, int> condition;
-
-condition s[maxN], ss[maxN];
-int t[maxN], d[maxN];
-map<int, int> f;
-int cnt;
-int l , n;
-
-
-
-bool check(int x) {
-    for (int i = 1; i <= x; ++i) {
-        ss[i] = s[i];
-    }
-    sort(ss+1, ss+x+1);
-    for (int i = 1; i <= cnt; ++i) d[i] = -1;
-    for (int i = 1; i <= x; ++i) {
-        int a = ss[i].first.first - 1;
-        int b = ss[i].first.second;
-        if (d[a] == -1 || d[b] == -1) {
-            if (d[a] != -1) d[b] = (d[a] + ss[i].second) % 2;
-            else if (d[b] != -1) d[a] = (d[b] + ss[i].second) % 2;
-            else {
-                d[a] = 0;
-                d[b] = ss[i].second;
-            }
-        }
-        else if ((d[a] + d[b]) % 2 != ss[i].second) return false;
-
-    }
-}
 
 int main() {
-    cin >> l >> n;
-    for (int i = 1; i <= n; ++i) {
-        string st;
-        cin >> s[i].first.first >> s[i].first.second >> st;
-        s[i].second = st == "odd";
-        f[s[i].first.first] = 0;
-        f[s[i].first.second] = 0;
-    }
-    cnt = 0;
-    for (auto& x : f) {
-        x.second = ++cnt;
-    }
-    for (int i = 1; i <= n; ++i) {
-        s[i].first.first = f[s[i].first.first];
-        s[i].first.second = f[s[i].first.second];
-    }
-    // for (int i = 1; i <= n; ++i) {cerr << s[i].first.first << " " << s[i].first.second << " " << s[i].second << endl;}
-    int L = 1, R = n;
-    int res = 0;
-    while (L <= R) {
-        int M = (L+R) / 2;
-        // cerr << M << endl;
-        if (check(M)) {
-            res = M;
-            L = M + 1;
-        }
-        else {
-            R = M - 1;
-        }
-    }
-    cout << res;
+    double a = 1000000000000000000;
+    double b = 1000000000000000000;
+    long long m = 1000000000000000000;
+    cout << (a * b) % m;
 }
--- timus1013.cpp
+++ timus1013.cpp
@@ -3,9 +3,19 @@ using namespace std;
 
 long long n, k , m;
 
+long long mul(long long a, long long b) {
+    if (b == 0) return 0;
+    if (b == 1) return a;
+    long long tmp = mul(a,b/2);
+    tmp = (tmp * 2) % m;
+    if (b%2) tmp = (tmp + a) % m;
+    return tmp;
+}
+
 
 struct matrix {
     long long arr[2][2];
+    matrix();
     matrix(long long a, long long b, long long c, long long d) {
         arr[0][0] = a % m ; arr[0][1] = b % m;
         arr[1][0] = c % m ; arr[1][1] = d & m;
@@ -16,14 +26,32 @@ struct matrix {
         for (int i = 0; i < 2; ++i) {
             for (int j = 0; j < 2; ++j) {
                 for (int k = 0; k < 2; ++k) 
-                    tmp.arr[i][j] = tmp.arr[i][j] + arr[i][k] * other.arr[k][j] % m)
+                    tmp.arr[i][j] = (tmp.arr[i][j] + mul(arr[i][k], other.arr[k][j])) % m;
             }
         }
+        return tmp;
+    }
+    matrix pow(long long x) {
+        if (x == 1) return (*this);
+        matrix tmp = (*this).pow(x/2);
+        tmp = tmp * tmp;
+        if (x%2) tmp = tmp * (*this);
+        return tmp;
+    }
+
+    void debug() {
+        cerr << arr[0][0] << " " << arr[0][1] << endl << arr[1][0] << " " << arr[1][1] << endl;
     }
 };
 
 
 
+
+
 int main() {
     scanf("%I64d%I64d%I64d", &n, &k, &m);
+    matrix a(k-1,k-1,1,0);
+    matrix b(k-1,0,1,0);
+    matrix c = a.pow(n-1) * b;
+    printf("%I64d", c.arr[0][0]);
 }
